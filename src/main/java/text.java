dacd615862ac76e33156d125c24dc1c0bff61f69
samirasophia <samira.tesan@coop.ch>
2023-10-31 15:43:38 +0100

    add code meow meow :3 meow meow nnnnn ;ggg

--- src/main/java/text.java
+++ src/main/java/text.java
@@ -2,6 +2,6 @@ public class text {
     public static void main(String[] args) {
         System.out.println("hallo db sfknvbs snvm.");
     }
-    String meow = "Anni isch gemein >:(";
+    String meow = "Anni isch geme>:(";
     String rawr;
 }
