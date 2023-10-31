90c1e934a8614e61d1f33edece11fb5a63b6cbd3
samirasophia <samira.tesan@coop.ch>
2023-10-31 13:57:36 +0100

    Setup Git

---
+++ Palindrom.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Palindrom {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Gib ein Wort ein: ");
+        String wort = sc.nextLine();
+
+        if (Palindrom(wort)) {
+            System.out.println(wort + " ist ein Palindrom.");
+        } else {
+            System.out.println(wort + " ist kein Palindro.");
+        }
+    }
+
+    public static boolean Palindrom(String wort) {
+        int laenge = wort.length();
+        for (int i = 0; i < laenge / 2; i++) {
+            if (wort.charAt(i) != wort.charAt(laenge - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
